daa6523be2f275638be89ca5e06e6e1f85df4a5d
SlavaGrach <grachv77@gmail.com>
2021-05-26 22:51:58 +0300

    Ученье свет

---
+++ src/fetchpikcers.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+//https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=query&page=номер_страницы&per_page=12&key=твой_ключ
+
+export default function fetchPicture(page, query) {
+    
+    return axios('?image_type=photo&orientation=horizontal&q=' + query + `&page=${page}&per_page=12&key=6482848-443bdcc87ab7c743d3374b0c5`).then(response => response.data.hits);
+}
